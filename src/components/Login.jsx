1ff7b156363425184ad4e4d4cbfd3561c083a050
Irving Amaechi <leoivin0@gmail.com>
2025-03-22 14:14:50 +0100

    Added a beta badge to logo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -57,8 +57,9 @@ const LoginPage = () => {
           {/* Form Section */}
           <div className="bg-white border-gray-200 p-6 w-full md:w-1/2">
             {/* Logo */}
-            <div className="text-center mb-9">
+            <div className="text-center mb-9 flex items-center justify-center">
               <h1 className="text-3xl font-bold font-archivo tracking-wide text-black">DATUS.</h1>
+              <span className="px-2 py-1 text-xs font-medium text-blue-600 bg-blue-100 rounded-full">BETA</span>
             </div>
 
             <div className="text-left mb-4">
